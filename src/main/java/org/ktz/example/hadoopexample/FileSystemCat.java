012ea2d88d4d62c5fbff1945781f8299cdc5c164
ktz <knightpop@gmail.com>
2017-01-16 17:32:19 +0900

    Chapt 3

---
+++ src/main/java/org/ktz/example/hadoopexample/FileSystemCat.java
@@ -0,0 +1,28 @@
+package org.ktz.example.hadoopexample;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+/**
+ * Created by ktz on 2017. 1. 16..
+ */
+public class FileSystemCat {
+    public static void main(String[] args) throws IOException {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        InputStream in = null;
+        try {
+            in = fs.open(new Path(uri));
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
