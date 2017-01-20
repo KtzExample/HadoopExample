97e6dd4b5a063a485ab29891355133f69a84e704
ktz <knightpop@gmail.com>
2017-01-20 17:16:51 +0900

    Chapt 3

--- src/main/java/org/ktz/example/hadoopexample/ListStatus.java
+++ src/main/java/org/ktz/example/hadoopexample/ListStatus.java
@@ -1,7 +1,9 @@
 package org.ktz.example.hadoopexample;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
@@ -18,6 +20,12 @@ public class ListStatus {
         Path[] paths = new Path[args.length];
         for(int i = 0; i < paths.length; i++){
             paths[i] = new Path(args[i]);
+        }
+
+        FileStatus[] status = fs.listStatus(paths);
+        Path[] listPaths = FileUtil.stat2Paths(status);
+        for(Path p : listPaths){
+            System.out.println(p);
         }
     }
 }
