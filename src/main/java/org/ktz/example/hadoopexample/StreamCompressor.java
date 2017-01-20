655dd99cba5289d2ccac730f649443537998f09d
ktz <knightpop@gmail.com>
2017-01-20 17:42:19 +0900

    add scalaScript

---
+++ src/main/java/org/ktz/example/hadoopexample/StreamCompressor.java
@@ -0,0 +1,10 @@
+package org.ktz.example.hadoopexample;
+
+/**
+ * Created by ktz on 2017. 1. 20..
+ */
+public class StreamCompressor {
+    public static void main(String[] args) throws Exception {
+        String codeClassname = args[0];
+    }
+}
